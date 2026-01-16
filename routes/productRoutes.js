a9361162bdae22e6b4eb350aec6e9277ea0aa7c7
Vansh Goel <vansh.eigenstudio@gmail.com>
2026-01-16 16:36:01 +0530

    added some products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,39 @@ const ensureAuth = require("../middlewares/auth");
 const router = require("express").Router();
 
 router.get("/", ensureAuth, (req, res) => {
-  res.send("Product route works!");
+  const products = [
+    {
+      id: "1",
+      name: "Laptop",
+      price: 999.99,
+      description: "High performance laptop with 16GB RAM"
+    },
+    {
+      id: "2",
+      name: "Smartphone",
+      price: 699.99,
+      description: "Latest smartphone with 5G support"
+    },
+    {
+      id: "3",
+      name: "Headphones",
+      price: 199.99,
+      description: "Wireless noise-cancelling headphones"
+    },
+    {
+      id: "4",
+      name: "Tablet",
+      price: 499.99,
+      description: "10-inch tablet perfect for productivity"
+    },
+    {
+      id: "5",
+      name: "Monitor",
+      price: 349.99,
+      description: "4K Ultra HD monitor for creative work"
+    }
+  ];
+  
+  res.json({ message: "Products fetched successfully", products });  
 });
 module.exports = router;
